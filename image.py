780f1b07169a8c5ad778196334d9faa6cb01d35f
saurabhsircar11 <saurabhsircar11@gmail.com>
2019-05-11 02:50:29 +0530

    Implemented bonarization

--- image.py
+++ image.py
@@ -1,7 +1,11 @@
 from PIL import Image
 import pytesseract
 
-im= Image.open("new_image.jpg")
-text= pytesseract.image_to_string(im,lang="eng")
+
+im = Image.open("german_prescription.png")
+gray = im.convert('L')
+bw = gray.point(lambda p: p > 50 and 255)
+bw.save("result_bw.png")
+text = pytesseract.image_to_string(bw, lang="eng")
 
 print(text)
